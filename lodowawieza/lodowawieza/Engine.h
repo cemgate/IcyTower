4fb734a5d0fef642cc27530db7804c56b909f3ef
Marcin Cichocki <79911405+cemgate@users.noreply.github.com>
2022-12-06 00:18:30 +0100

    Add files via upload
    Physics engine - DONE

---
+++ lodowawieza/lodowawieza/Engine.h
@@ -0,0 +1,14 @@
+/*#pragma once
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include<iostream>
+#include<vector>
+#include <string>
+class Engine
+{
+	move_r();
+	move_l();
+	move_u();
+	move_d();
+};
+
